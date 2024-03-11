3b73568e968ff6e4f4ff82a50863519f79e82f2c
kualta <contact@kualta.dev>
2024-03-12 00:15:43 +0300

    add priveleged runs to watchtower

--- services/action.sh
+++ services/action.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp -f ~/bits/services/system/* /etc/systemd/system/
+sudo cp -f ~/bits/services/system/* /etc/systemd/system/
 
 # reload systemd
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
 # restart each service
 while IFS= read -r service; do
-    systemctl restart "$service.service"
+    sudo systemctl restart "$service.service"
 done < <(jq -r '.services[]' ~/bits/services/services.json)
