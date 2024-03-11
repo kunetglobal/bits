c40a75ac9a461ee5d9862cc56393d0c55e2d6958
kualta <contact@kualta.dev>
2024-03-12 00:19:38 +0300

    add watchtower report messages

--- services/action.sh
+++ services/action.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo cp -f ~/bits/services/system/* /etc/systemd/system/
+echo "Watchtower: Service filed updated"
 
-# reload systemd
 sudo systemctl daemon-reload
+echo "Watchtower: Service daemon reloaded"
 
-# restart each service
 while IFS= read -r service; do
     sudo systemctl restart "$service.service"
 done < <(jq -r '.services[]' ~/bits/services/services.json)
+echo "Watchtower: Restarted all services"
