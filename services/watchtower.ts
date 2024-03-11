96c321f10e220fdff7cebe89b1407839cd491ebd
kualta <contact@kualta.dev>
2024-03-11 16:44:02 +0300

    update watchtower

--- services/watchtower.ts
+++ services/watchtower.ts
@@ -4,20 +4,49 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.WATCHTOWER_PORT;
+const secret = process.env.GITHUB_WEBHOOK_SECRET;
+
+if (!secret) throw new Error("GITHUB_WEBHOOK_SECRET is not defined");
+if (!port) throw new Error("WATCHTOWER_PORT is not defined");
 
 app.use(express.json());
 
 app.post("/hook", (req, res) => {
 	const { body } = req;
 
-	// Check if the request is from GitHub
 	const githubSignature = req.headers["x-hub-signature"] as string;
-	const isGithubEvent = req.headers["x-github-event"] === "push";
-	const secret = process.env.GITHUB_WEBHOOK_SECRET;
+	if (!githubSignature) {
+		console.error("GitHub signature is not defined", req.headers);
+		return res
+			.status(400)
+			.send("Invalid request: GitHub signature is not defined");
+	}
 
-	if (!githubSignature || !isGithubEvent || !secret) {
-		return res.status(400).send("Invalid request");
+	const githubEvent = req.headers["x-github-event"] as string;
+	const isGithubPushEvent = githubEvent === "push";
+
+	if (!isGithubPushEvent) {
+		console.log("Ignoring event", githubEvent, "Reason: not a push event");
+		return res.status(200).send("Webhook received successfully");
+	}
+
+	// Verify signature
+	verifySignature(req, res);
+
+	// Trigger action
+	console.log("New commit pushed:", body.head_commit.id);
+
+	res.status(200).send("Webhook received successfully");
+});
+
+function verifySignature(req: express.Request, res: express.Response) {
+	const githubSignature = req.headers["x-hub-signature"] as string;
+	if (!githubSignature) {
+		console.error("GitHub signature is not defined", req.headers);
+		return res
+			.status(400)
+			.send("Invalid request: GitHub signature is not defined");
 	}
 
 	// Verify signature
@@ -26,22 +55,17 @@ app.post("/hook", (req, res) => {
 	const payload = JSON.stringify(req.body);
 	const expectedSignature = `sha1=${hmac.update(payload).digest("hex")}`;
 	if (
-		crypto.timingSafeEqual(
+		!crypto.timingSafeEqual(
 			Buffer.from(githubSignature),
 			Buffer.from(expectedSignature),
 		)
 	) {
-		console.log("GitHub signature verified");
-
-		// Trigger action
-		console.log("New commit pushed:", body.head_commit.id);
-
-		res.status(200).send("Webhook received successfully");
-	} else {
-		console.log("GitHub signature verification failed");
+		console.error("GitHub signature verification failed");
 		res.status(400).send("GitHub signature verification failed");
+	} else {
+		console.log("GitHub signature verified");
 	}
-});
+}
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
