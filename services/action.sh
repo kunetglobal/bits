9711aeb9bc699e5820912465421250e794064644
kualta <contact@kualta.dev>
2024-03-12 00:02:06 +0300

    fix watchtower actions script

--- services/action.sh
+++ services/action.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -f ./system/* /etc/systemd/system/
+cp -f ~/bits/services/system/* /etc/systemd/system/
 
 # reload systemd
 systemctl daemon-reload
@@ -8,4 +8,4 @@ systemctl daemon-reload
 # restart each service
 while IFS= read -r service; do
     systemctl restart "$service.service"
-done < <(jq -r '.services[]' ./services.json)
+done < <(jq -r '.services[]' ~/bits/services/services.json)
