419ae276abef0cb317846717579bdf3572aa7923
kualta <contact@kualta.dev>
2024-03-13 12:39:16 +0300

    refactor watchtower

--- services/watchtower.ts
+++ services/watchtower.ts
@@ -1,61 +1,53 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { exec } from "child_process";
-
+import { exec, ExecException } from "child_process";
 import { promisify } from "util";
 
 dotenv.config();
 const app = express();
 const execPromise = promisify(exec);
-const port = process.env.WATCHTOWER_PORT;
-const secret = process.env.GITHUB_WEBHOOK_SECRET;
+const port: string | undefined = process.env.WATCHTOWER_PORT;
+const secret: string | undefined = process.env.GITHUB_WEBHOOK_SECRET;
 
 if (!secret) throw new Error("GITHUB_WEBHOOK_SECRET is not defined");
 if (!port) throw new Error("WATCHTOWER_PORT is not defined");
 
 app.use(express.json());
 
-app.post("/hook", (req, res) => {
-	const { body } = req;
+app.post("/hook", (req: Request, res: Response) => {
+	// Only listen to push events
+	checkEventType(req, res);
 
-	const githubSignature = req.headers["x-hub-signature"] as string;
-	if (!githubSignature) {
-		console.error("GitHub signature is not defined", req.headers);
-		return res
-			.status(400)
-			.send("Invalid request: GitHub signature is not defined");
-	}
-
-	const githubEvent = req.headers["x-github-event"] as string;
-	const isGithubPushEvent = githubEvent === "push";
-
-	if (!isGithubPushEvent) {
-		console.log("Ignoring event", githubEvent, "Reason: not a push event");
-		return res.status(200).send("Webhook received successfully");
-	}
-
+	// Make sure we're communicating with github
 	verifySignature(req, res);
 
-	// Log the update
-	console.log("New commit pushed:", body.head_commit.id);
-
-	runWatchtowerActions(res);
+	// Pull new code and run the watchtower script
+	runWatchtowerActions(req);
 });
 
-function verifySignature(req: express.Request, res: express.Response) {
-	const githubSignature = req.headers["x-hub-signature"] as string;
+function checkEventType(req: Request, res: Response): void {
+	const githubEvent: string = req.headers["x-github-event"] as string;
+	const isGithubPushEvent: boolean = githubEvent === "push";
+
+	if (!isGithubPushEvent) {
+		console.log("Dismissing event", githubEvent, "Reason: not a push event");
+		res.status(204).send("Dismissed event");
+	}
+}
+
+function verifySignature(req: Request, res: Response): void {
+	const githubSignature: string | undefined = req.headers["x-hub-signature"] as string | undefined;
 	if (!githubSignature) {
 		console.error("GitHub signature is not defined", req.headers);
-		return res
-			.status(400)
-			.send("Invalid request: GitHub signature is not defined");
+		res.status(400).send("Invalid request: GitHub signature is not defined");
+		return;
 	}
 
 	// Verify signature
 	const crypto = require("crypto");
 	const hmac = crypto.createHmac("sha1", secret);
-	const payload = JSON.stringify(req.body);
-	const expectedSignature = `sha1=${hmac.update(payload).digest("hex")}`;
+	const payload: string = JSON.stringify(req.body);
+	const expectedSignature: string = `sha1=${hmac.update(payload).digest("hex")}`;
 	if (
 		!crypto.timingSafeEqual(
 			Buffer.from(githubSignature),
@@ -66,10 +58,13 @@ function verifySignature(req: express.Request, res: express.Response) {
 		res.status(400).send("GitHub signature verification failed");
 	} else {
 		console.log("GitHub signature verified");
+		res.status(200).send("Received successfully");
 	}
 }
 
-async function runWatchtowerActions(res: express.Response) {
+async function runWatchtowerActions(req: Request): Promise<void> {
+	console.log("New commit pushed:", req.body.head_commit?.id);
+
 	try {
 		// Pull latest code from GitHub
 		const gitPull = await execPromise("cd ~/bits && git pull");
@@ -80,11 +75,8 @@ async function runWatchtowerActions(res: express.Response) {
 		const actionScript = await execPromise("~/bits/services/action.sh");
 		if (actionScript.stdout) console.log(actionScript.stdout);
 		if (actionScript.stderr) console.error(actionScript.stderr);
-
-		res.status(200).send("Webhook received successfully");
 	} catch (error) {
 		console.error(error);
-		res.status(500).send("Internal server error");
 	}
 }
 
