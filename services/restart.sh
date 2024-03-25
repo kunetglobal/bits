30f89269932e4ae6ea3ec4ff3451d3a4aeca182a
kualta <contact@kualta.dev>
2024-03-25 19:47:45 +0300

    fix service bootloop

--- services/restart.sh
+++ services/restart.sh
@@ -8,15 +8,17 @@ echo "Watchtower: Service files updated"
 sudo systemctl daemon-reload
 echo "Watchtower: Service daemon reloaded"
 
-echo "Watchtower: Restarting all"
+echo "Watchtower: Restarting all services"
 script_dir=$(dirname "$0")
 for service_file in "$script_dir/system/"*.service; do
     file=$(basename "$service_file" .service)
 
-    sudo systemctl restart "$file.service"
-    sudo systemctl enable "$file.service"
-    sudo systemctl start "$file.service"
-    echo " - restarted $file"
+    if [ "$file" != "restart" ]; then
+        sudo systemctl restart "$file.service"
+        sudo systemctl enable "$file.service"
+        sudo systemctl start "$file.service"
+        echo " - restarted $file"
+    fi
 
 done
 echo "Watchtower: Restarted all services"
